bb244f06baf065c0bd7c6f992626c2dc0dc778e8
redcoder23 <130477374+redcoder23@users.noreply.github.com>
2025-03-18 21:45:47 +0530

    Added lowercase conversion feature

--- src/COMPONENTS/TextForm.js
+++ src/COMPONENTS/TextForm.js
@@ -10,6 +10,12 @@ export default function TextForm(props) {
     const handleOnChange = (event) => {
         console.log(event);
         settext(event.target.value);
+    }  
+
+    const handleloclick =() =>  
+    {
+        let nextext=text.toLowerCase(); 
+        settext(nextext); 
     }
 
     const [text, settext] = useState('enter text here');
@@ -17,21 +23,27 @@ export default function TextForm(props) {
     // settext("new text"); //correct way to change the state 
     return (
         <>
-            <div className="container">
+            <div className="container">,,,,,
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange}
                         id="mybox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary" onClick={handleUpClick}>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>
                     convert to uppercase
-                </button>
-            </div>  
-        
-            <div className="container my-3">  
-             <h1>Your text summary</h1> 
-             <p>{text.split(" ").length} words and {text.length} characters</p>  
-             <p>{0.008*text.split(" ").length} minutes required to read it</p>
+                </button>    
+                 
+                 <button className="btn btn-primary mx-1" onClick={handleloclick}> 
+                    convert to LowerCase
+                 </button>
+            </div>
+
+            <div className="container my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} minutes required to read it</p>
+                <h2>preview</h2>
+                <p>{text}</p>
             </div>
         </>
     )
